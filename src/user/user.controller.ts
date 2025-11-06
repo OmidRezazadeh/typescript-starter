48d1793195d1a84024a7319931f1a69bf4ab96bd
orprogrammer <orprogrammer@gmail.com>
2025-11-06 15:01:45 +0330

    user find

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -8,6 +8,10 @@ export class UserController {
   async create(
     @Body() data: { name: string; email: string; password: string },
   ) {
-    return await this.userService.create(data);
+    return await this.userService.create(data); 
   }
+@Get('list/:userId')
+async list(@Param('userId', ParseIntPipe) userId: number) {
+  return this.userService.list(userId);
+}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,4 +7,9 @@ export class UserService {
   async create(data: { name: string; email: string; password: string }) {
     return await this.prisma.user.create({ data });
   }
+ async list(userId: number) {
+  return this.prisma.user.findUnique({
+    where: { id: userId },
+  });
+}
 }
